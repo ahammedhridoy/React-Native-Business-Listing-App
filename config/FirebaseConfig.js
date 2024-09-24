a8fb3fa2526b5a718849ea2186fb90b8cad79f99
ahammedhridoy <developerhridoy@gmail.com>
2024-09-24 17:34:37 +0600

    env updated

--- config/FirebaseConfig.js
+++ config/FirebaseConfig.js
@@ -2,17 +2,25 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+} from "@env";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDV0FIkI2o3rNUNO2M6-DEAI7mdcbVMKXE",
-  authDomain: "business-directory-app-3572d.firebaseapp.com",
-  projectId: "business-directory-app-3572d",
-  storageBucket: "business-directory-app-3572d.appspot.com",
-  messagingSenderId: "228214962545",
-  appId: "1:228214962545:web:0745ae030dbe93559b5ed0",
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
 };
 
 // Initialize Firebase
